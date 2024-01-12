81896491d9dcb4ca8a7a374d82639d316203c5b4
Alejandro Meza <alemz-li@outlook.com>
2024-01-11 20:42:29 -0800

    Added build steps for Render hosting of Application

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5000/api",
-  // baseURL: "http://192.168.0.20:5000/api",
+  baseURL: "/api",
 });
--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express, { Request, Response } from "express";
 import morgan from "morgan";
 import tmdbRoutes from "./routes/tmdb.routes";
 import cors from "cors";
+import { join } from "path";
 
 const app = express();
 
@@ -10,7 +11,15 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 
+console.log(__dirname);
+// Frontend
+app.use(express.static(join(__dirname, "../client/dist")));
+
 app.use("/api/tmdb", tmdbRoutes);
+
+app.all("*", (_req: Request, res: Response) => {
+  res.sendFile(join(__dirname, "../client/dist/index.html"));
+});
 
 app.use((_req: Request, res: Response) => {
   res.status(404).json({ message: "Not Found" });
